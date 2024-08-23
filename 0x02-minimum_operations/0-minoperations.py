5867a7899bb598909b1c3632c1e0909c3d3560a8
ABANGTANYI <abangtanyia@gmail.com>
2024-08-22 19:18:12 -0700

    Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,38 +1,19 @@
 #!/usr/bin/env python3
-
-"""
-Calculates the minimum number of 'Copy All' and 'Paste' operations
-needed to generate n 'H' characters in a text file.
-
-Args:
-    n (int): The target number of 'H' characters to generate.
-
-Returns:
-    int: The minimum number of operations needed to generate n 'H' characters.
-         If n is impossible to achieve, returns 0.
-"""
-def minOperation(n):
-    if n <= 0:
+# This function calculates the minimum number of "Copy All" and "Paste" operations
+# needed to achieve a given number of 'H' characters in a text file.
+def minOperations(n):
+    if n <= 1:
         return 0
-    
-    print("n before loop", n)
-    
-    divisor = 2
-    num_of_operations = 0
-    
-    while n > 1:
-        if n % divisor == 0:
-            print("divisor is : ", divisor)
-            n = n / divisor
-            print("\nn = n / divisor", n)
-            
-            num_of_operations += 1
-            print("num of ops", num_of_operations)
-        else:
-            divisor += 1
-            print("divisor after increment:", divisor)
-    
-    return num_of_operations
 
-n = 12
-print(minOperation(n))
+    # Find the largest power of 2 that is less than or equal to n
+    power_of_2 = 1
+    while power_of_2 * 2 <= n:
+        power_of_2 *= 2
+
+    # If n is a power of 2, return the number of operations needed
+    if n == power_of_2:
+        return n // 1
+
+    # If n is not a power of 2, find the smallest power of 2 that is greater than n
+    # and subtract the largest power of 2 that is less than or equal to n
+    return (n - power_of_2) // (power_of_2 // 2) + n // power_of_2
