52dd1587f8c8098c1fbca084844b34ebac74b8d1
ABANGTANYI <abangtanyia@gmail.com>
2024-08-22 19:21:44 -0700

    Task 0

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,19 +1,17 @@
-#!/usr/bin/env python3
-# This function calculates the minimum number of "Copy All" and "Paste" operations
-# needed to achieve a given number of 'H' characters in a text file.
+#!/usr/bin/python3
+"""
+Minimum operations
+"""
+
+
 def minOperations(n):
+    """
+
+    :param n:
+    :return:
+    """
     if n <= 1:
         return 0
-
-    # Find the largest power of 2 that is less than or equal to n
-    power_of_2 = 1
-    while power_of_2 * 2 <= n:
-        power_of_2 *= 2
-
-    # If n is a power of 2, return the number of operations needed
-    if n == power_of_2:
-        return n // 1
-
-    # If n is not a power of 2, find the smallest power of 2 that is greater than n
-    # and subtract the largest power of 2 that is less than or equal to n
-    return (n - power_of_2) // (power_of_2 // 2) + n // power_of_2
+    for op in range(2, n+1):
+        if n % op == 0:
+            return minOperations(int(n/op)) + op
