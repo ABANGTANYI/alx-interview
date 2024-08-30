590599792a545dc7e2389f8c78769788dcc0cd96
ABANGTANYI <abangtanyia@gmail.com>
2024-08-29 17:03:17 -0700

    Task 0

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,42 +1,39 @@
 #!/usr/bin/python3
+"""
+Log parsing
+"""
+
 import sys
-import re
 
-def output(log):
-    print(f"File size: {log['file_size']}")
-    for code in sorted(log['code_frequency']):
-        if log['code_frequency'][code]:
-            print(f"{code}: {log['code_frequency'][code]}")
+if __name__ == '__main__':
 
-def parse_log_line(line, log):
-    pattern = re.compile(r'^(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}) - \[(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})\] "GET /projects/260 HTTP/1.1" (\d{3}) (\d+)$')
-    match = pattern.match(line)
-    if match:
-        code = match.group(3)
-        file_size = int(match.group(4))
+    filesize, count = 0, 0
+    codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
+    stats = {k: 0 for k in codes}
 
-        log['file_size'] += file_size
-
-        if code.isdigit() and int(code) in [200, 301, 400, 401, 403, 404, 405, 500]:
-            log['code_frequency'][code] += 1
-
-def main():
-    log = {
-        'file_size': 0,
-        'code_frequency': {str(code): 0 for code in [200, 301, 400, 401, 403, 404, 405, 500]}
-    }
-    line_count = 0
+    def print_stats(stats: dict, file_size: int) -> None:
+        print("File size: {:d}".format(filesize))
+        for k, v in sorted(stats.items()):
+            if v:
+                print("{}: {}".format(k, v))
 
     try:
         for line in sys.stdin:
-            line = line.strip()
-            parse_log_line(line, log)
-            line_count += 1
-
-            if line_count % 10 == 0:
-                output(log)
+            count += 1
+            data = line.split()
+            try:
+                status_code = data[-2]
+                if status_code in stats:
+                    stats[status_code] += 1
+            except BaseException:
+                pass
+            try:
+                filesize += int(data[-1])
+            except BaseException:
+                pass
+            if count % 10 == 0:
+                print_stats(stats, filesize)
+        print_stats(stats, filesize)
     except KeyboardInterrupt:
-        output(log)
-
-if __name__ == "__main__":
-    main()
+        print_stats(stats, filesize)
+        raise
